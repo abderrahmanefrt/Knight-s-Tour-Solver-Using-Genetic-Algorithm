5b2b6215687501c4a5b2d9dee9b8c0b8014035ad
abderrahmanefrt <abderrahmaneferhat93@gmail.com>
2025-11-15 17:22:06 +0100

    chromosom class

--- KnightTour.py
+++ KnightTour.py
@@ -2,12 +2,42 @@ import random
 
 
 class Chromosome:
-   genes= []
+  
    def __init__(self, genes):
        if genes is None:
-           genes= random.sample(range(64),64)
+            self.genes = [random.randint(1, 8) for _ in range(63)]
        else:
            self.genes=genes
-    
 
-      
+   
+   def crossover (self,partner):
+       
+       point=random.randint(1, 62)
+
+       genes1=self.gens[:point]+ partner.genes[point:]
+       genes2=partner.genes[:point]+ self.genes[point:]
+
+       return Chromosome(genes1), Chromosome(genes2)
+       
+
+   def mutation(self,mutation_rate ):
+       for i in range(len(self.genes)):
+           if random.random()< mutation_rate:
+               self.genes[i]= random.randint(1,8)
+
+       
+class Knightclass:
+    position =(0,0)
+    Chromosome= None
+    path= []
+    fitness= 0
+
+    def __int__(self,Chromosome):
+        
+        self.Chromosome= Chromosome
+        self.position= (0,0)
+        self.path= [self.position]
+        self.fitness=0
+        
+        
+        
